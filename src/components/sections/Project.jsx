3136c8284a1e652827d312feaf3d936bd07f4114
apeksha-rgb <apeksha.manshi.shakya@gmail.com>
2025-02-07 22:11:10 +0530

    first commit

---
+++ src/components/sections/Project.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <section id='project' className='min-h-screen flex items-center justify-center py-20'>
+      <div className='max-w-5xl mx-auto px-4'></div>
+        <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500  to-cyan-400 bg-clip-text text-transparent text-center'>
+          Featured Projects
+        </h2>
+        <div className='grid grid-cols-2 md:grid-cols-2 gap-6'>
+
+          <div></div>
+        </div>
+    </section>
+  )
+}
+
+export default Project
